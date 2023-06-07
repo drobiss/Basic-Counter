670f4fb9b565c00c3af763d917cb99608eb7d0f3
drobiss <103402088+drobiss@users.noreply.github.com>
2023-06-07 14:19:39 +0200

    add basic counter files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    
+    countEl.textContent = 0
+    count = 0
+}
+
+
