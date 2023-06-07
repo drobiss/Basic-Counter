3cf7e3d95a211e8c627fd5fd8d22df044c413e4c
drobiss <103402088+drobiss@users.noreply.github.com>
2023-06-07 19:24:58 +0200

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// 1. Grab the save-el paragrah and store it in a variable called saveEl
 let countEl = document.getElementById("count-el")
 let count = 0
 let saveEl = document.getElementById("save-el")
